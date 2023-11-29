cbd203bdcec53fbe3e396cfd6edb86cc5ade43c7
Noah Overcash <novercash@ebsco.com>
2023-11-29 16:49:14 -0500

    oops

--- src/main/resources/db/changelog/changes/v1.0.4/remove_entity_type_label_aliases.sql
+++ src/main/resources/db/changelog/changes/v1.0.4/remove_entity_type_label_aliases.sql
@@ -5,7 +5,7 @@ UPDATE entity_type_definition src
     SELECT
       (src.definition::jsonb #- '{labelAlias}') -- remove root labelAlias
       || jsonb_build_object('columns', jsonb_agg(elem - 'labelAlias')) -- remove inner column labelAlias, concatenating back
-    FROM fs09000000_mod_fqm_manager.entity_type_definition
+    FROM entity_type_definition
     -- must unroll the array and iterate; no convenient operator :(
     -- https://stackoverflow.com/a/69482108/4236490
     CROSS JOIN lateral jsonb_array_elements(definition::jsonb#>'{columns}') as arr(elem) where src.id = id

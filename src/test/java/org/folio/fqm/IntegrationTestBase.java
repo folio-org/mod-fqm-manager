ada19f9e226560ec114a585987398c38fad3eeab
Matt Weaver <mweaver@ebsco.com>
2024-02-01 13:26:40 -0500

    MODFQMMGR-116 Add integration test for valueFunctions

---
+++ src/test/java/org/folio/fqm/IntegrationTestBase.java
@@ -0,0 +1,123 @@
+package org.folio.fqm;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.junit.jupiter.Container;
+
+import javax.sql.DataSource;
+import java.nio.file.Path;
+import java.time.Duration;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class IntegrationTestBase {
+
+  private static final Logger LOG = LoggerFactory.getLogger(IntegrationTestBase.class);
+  private static final boolean IS_LOG_ENABLED = false;  // for debugging set to true!
+  private static final Network network = Network.newNetwork();
+
+  public static final String TENANT_ID = "beeuni";
+
+  @Container
+  private static final PostgreSQLContainer<?> postgres =
+    new PostgreSQLContainer<>("postgres:12-alpine")
+      .withNetwork(network)
+      .withNetworkAliases("mypostgres")
+      .withExposedPorts(5432)
+      .withUsername("myusername")
+      .withPassword("mypassword")
+      .withDatabaseName("mypostgres");
+
+  @Container
+  private static final GenericContainer<?> module =
+    new GenericContainer<>(
+      new ImageFromDockerfile("mod-fqm-manager").withFileFromPath(".", Path.of(".")))
+      .withNetwork(network)
+      .withExposedPorts(8081)
+      .withEnv("DB_HOST", "mypostgres")
+      .withEnv("DB_PORT", "5432")
+      .withEnv("DB_USERNAME", "myusername")
+      .withEnv("DB_PASSWORD", "mypassword")
+      .withEnv("DB_DATABASE", "mypostgres")
+      .withStartupTimeout(Duration.ofMinutes(3))
+      .dependsOn(postgres);
+
+  @BeforeAll
+  public static void beforeAll() {
+    // General integration test config
+    RestAssured.reset();
+    RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    RestAssured.baseURI = "http://" + module.getHost() + ":" + module.getFirstMappedPort();
+    if (IS_LOG_ENABLED) {
+      module.followOutput(new Slf4jLogConsumer(LOG).withSeparateOutputStreams());
+    }
+
+    // Bootstrap the module's DB
+    String body = """
+                  {"module_to": "999999.0.0",
+                   "parameters": [{"key": "loadReference", "value": "true"},
+                                  {"key": "loadSample",    "value": "true"} ]
+                  }
+                  """;
+    postTenant(body);
+
+    // Run some quick tests. These are done outside actual @Tests, since we need to make sure the service is up and
+    // running before any other integration tests run
+
+    // Test: migrate from 0.0.0, migration should be idempotent
+    body = """
+           {"module_from": "0.0.0",
+           """ + body.substring(1);
+    postTenant(body);
+
+    // Test: smoke test the /entity-types endpoint to verify DB interactions are working
+    smokeTest();
+  }
+
+  @BeforeEach
+  public void beforeEach() {
+    RestAssured.requestSpecification = null;
+  }
+
+  protected static DataSource getDataSource() {
+    final HikariConfig config = new HikariConfig();
+    config.setJdbcUrl(postgres.getJdbcUrl());
+    config.setUsername(postgres.getUsername());
+    config.setPassword(postgres.getPassword());
+    return new HikariDataSource(config);
+  }
+
+  private static void postTenant(String body) {
+    given()
+      .header("X-Okapi-Tenant", TENANT_ID)
+      .contentType("application/json")
+      .body(body)
+      .when()
+      .post("/_/tenant")
+      .then()
+      .statusCode(204);
+  }
+
+  private static void smokeTest() {
+    given()
+      .header("X-Okapi-Tenant", TENANT_ID)
+      .contentType("application/json")
+      .when()
+      .get("/entity-types")
+      .then()
+      .statusCode(200)
+      .body("$.size()", is(4));
+  }
+
+}
---
+++ src/test/java/org/folio/fqm/service/FqlToSqlConverterServiceIT.java
@@ -0,0 +1,95 @@
+package org.folio.fqm.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.folio.fqm.IntegrationTestBase;
+import org.folio.querytool.domain.dto.EntityType;
+import org.folio.querytool.domain.dto.EntityTypeColumn;
+import org.folio.querytool.domain.dto.RangedUUIDType;
+import org.folio.querytool.domain.dto.StringType;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@Testcontainers
+class FqlToSqlConverterServiceIT extends IntegrationTestBase {
+
+  @Test
+  void valueFunctionTest() throws JsonProcessingException {
+
+    // Given this dummy entity type with built-in mock data (including a column with a filterValueGetter and valueFunction)
+    EntityType entityType = new EntityType()
+      .id(UUID.randomUUID().toString())
+      .name("test")
+      .labelAlias("test alias")
+      .root(true)
+      ._private(false)
+      .columns(List.of(
+        new EntityTypeColumn()
+          .name("id")
+          .dataType(new RangedUUIDType())
+          .labelAlias("id alias")
+          .visibleByDefault(true)
+          .valueGetter("id"),
+        new EntityTypeColumn()
+          .name("some_column")
+          .dataType(new StringType())
+          .labelAlias("some column")
+          .visibleByDefault(true)
+          .valueGetter("some_column")
+          .filterValueGetter("lower(\"left\"(some_column, 10))")
+          .valueFunction("lower(\"left\"(:value, 10))")
+      ))
+      .fromClause("""
+        (values
+          ('2af997b6-2655-459e-bdca-decbf54795ae'::uuid, 'AbCdEfGhIjKlMnOpQrStUvWxYz', 456),
+          ('e0e4233e-fea0-4834-96ac-78739a1856d3'::uuid, 'blah blah blah', 789)
+        ) as t (id, some_column, unused_column)""");
+
+    var json = new ObjectMapper().writeValueAsString(entityType);
+    var sql = """
+      insert into %s_mod_fqm_manager.entity_type_definition
+        (id, derived_table_name, definition) values
+        (:id, :name, :definition::json)
+      """.formatted(TENANT_ID);
+
+    NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
+    jdbcTemplate.update(sql, Map.of("id", UUID.fromString(entityType.getId()), "name", entityType.getName(), "definition", json));
+
+    // When we query for a value that only actually matches the mock data when it gets run through the valueFunction
+    // and is compared against the value produced by the filterValueGetter
+    String fql = """
+      {
+        "some_column": { "$eq": "aBcDeFgHiJ...and nothing else matters after 10 chars. Note that the capitalization is different than in the 'real' value, too" }
+      }""";
+
+    // Then we get back the 1 expected row
+    given()
+      .header("X-Okapi-Tenant", TENANT_ID)
+      .contentType("application/json")
+      .when()
+      .queryParams(
+        "query", fql,
+        "entityTypeId", entityType.getId(),
+        "fields", List.of("some_column")
+      )
+      .get("/query")
+      .then()
+      .statusCode(200)
+      .body(
+        // Of the 2 possible results, only 1 should match, due to the filterValueGetter and valueFilter (despite the
+        // query not matching what's in the DB)
+        "content.size()", is(1),
+        // Also, the returned value should exactly match what's in the DB, not the truncated lower-case one from the
+        // filterValueGetter or valueGetter
+        "content[0].some_column", is("AbCdEfGhIjKlMnOpQrStUvWxYz")
+      );
+  }
+}

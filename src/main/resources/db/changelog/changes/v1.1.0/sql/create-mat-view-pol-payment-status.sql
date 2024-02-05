f794dee04bab71146d77db51ad237f5e0393414e
Bobby Sharp <bsharp@ebsco.com>
2024-02-05 10:59:28 -0500

    Add unique indexes and hardcoded statuses to new materialized views

--- src/main/resources/db/changelog/changes/v1.1.0/sql/create-mat-view-pol-payment-status.sql
+++ src/main/resources/db/changelog/changes/v1.1.0/sql/create-mat-view-pol-payment-status.sql
@@ -1,5 +1,19 @@
-CREATE MATERIALIZED VIEW IF NOT EXISTS drv_pol_payment_status AS
-SELECT DISTINCT jsonb ->> 'paymentStatus' AS payment_status
-FROM ${tenant_id}_mod_orders_storage.po_line;
+DROP MATERIALIZED VIEW IF EXISTS drv_pol_payment_status;
 
-REFRESH MATERIALIZED VIEW drv_pol_payment_status;
+CREATE MATERIALIZED VIEW drv_pol_payment_status AS
+SELECT payment_status FROM
+	(SELECT DISTINCT jsonb ->> 'paymentStatus' AS payment_status FROM ${tenant_id}_mod_orders_storage.po_line) existing_statuses
+UNION
+	(SELECT payment_status FROM
+	 	(VALUES
+			('Awaiting Payment'),
+			('Cancelled'),
+			('Fully Paid'),
+			('Partially Paid'),
+			('Payment Not Required'),
+			('Pending')
+		) AS hardcoded_statuses(payment_status)
+	);
+
+CREATE UNIQUE INDEX fqm_pol_payment_status
+ON ${tenant_id}_mod_fqm_manager.drv_pol_payment_status(payment_status);

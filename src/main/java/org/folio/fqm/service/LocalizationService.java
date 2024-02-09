5d60ee73a25d553633a6e05348e453e7ef71f0da
Noah Overcash <novercash@ebsco.com>
2024-02-09 10:50:07 -0500

    [MODFQMMGR-150] [MODFQMMGR-159] Support localization of custom fields and deeply nested object properties (#129)
    * [MODFQMMGR-150] Support localization of custom fields and deeply nested properties
    
    * formatting
    
    * eol
    
    * changelog update
    
    * test fix
    
    * typo fix
    
    * Coverage improvement

--- src/main/java/org/folio/fqm/service/LocalizationService.java
+++ src/main/java/org/folio/fqm/service/LocalizationService.java
@@ -1,40 +1,106 @@
 package org.folio.fqm.service;
 
+import lombok.AllArgsConstructor;
+import org.folio.querytool.domain.dto.ArrayType;
+import org.folio.querytool.domain.dto.EntityType;
+import org.folio.querytool.domain.dto.EntityTypeColumn;
+import org.folio.querytool.domain.dto.ObjectType;
 import org.folio.spring.i18n.service.TranslationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- * Small wrapper class for {@link TranslationService TranslationService} to provide reusable templates for
- * translations, particularly for entity type definitions.
+ * Small wrapper class for {@link TranslationService TranslationService} to provide reusable templates for translations,
+ * particularly for entity type definitions.
  */
 @Service
+@AllArgsConstructor(onConstructor_ = @Autowired)
 public class LocalizationService {
 
-  private static final String ENTITY_TYPE_LABEL_TRANSLATION_TEMPLATE =
-    "mod-fqm-manager.entityType.%s";
-  private static final String ENTITY_TYPE_COLUMN_LABEL_TRANSLATION_TEMPLATE =
-    "mod-fqm-manager.entityType.%s.%s";
+  // refers to the entity type as a whole in plural, e.g. "Users", "Purchase order lines"
+  private static final String ENTITY_TYPE_LABEL_TRANSLATION_TEMPLATE = "mod-fqm-manager.entityType.%s";
+  // refers to a single column inside the entity type, e.g. "Name", "Barcode"
+  private static final String ENTITY_TYPE_COLUMN_LABEL_TRANSLATION_TEMPLATE = "mod-fqm-manager.entityType.%s.%s";
+  // refers to a property inside an objectType column inside the entity type, e.g. "City" inside "Address" column inside "Users"
+  private static final String ENTITY_TYPE_COLUMN_NESTED_LABEL_TRANSLATION_TEMPLATE =
+    "mod-fqm-manager.entityType.%s.%s.%s";
+  // refers to a possessive version of the entity type, for custom fields, e.g. "User's {customField}"
+  private static final String ENTITY_TYPE_CUSTOM_FIELD_POSSESSIVE_TRANSLATION_TEMPLATE =
+    "mod-fqm-manager.entityType.%s._custom_field_possessive";
+
+  // the translation parameter for custom fields
+  private static final String CUSTOM_FIELD_PARAMETER = "customField";
 
   private TranslationService translationService;
 
-  @Autowired
-  public LocalizationService(TranslationService translationService) {
-    this.translationService = translationService;
+  public EntityType localizeEntityType(EntityType entityType) {
+    entityType.setLabelAlias(getEntityTypeLabel(entityType.getName()));
+
+    entityType.getColumns().forEach(column -> localizeEntityTypeColumn(entityType, column));
+
+    return entityType;
+  }
+
+  public void localizeEntityTypeColumn(EntityType entityType, EntityTypeColumn column) {
+    // Custom field names are already localized as they are user-defined, so they require special handling
+    if (Boolean.TRUE.equals(column.getIsCustomField())) {
+      column.setLabelAlias(getEntityTypeCustomFieldLabel(entityType.getName(), column.getName()));
+      return;
+    }
+
+    column.setLabelAlias(getEntityTypeColumnLabel(entityType.getName(), column.getName()));
+
+    if (column.getDataType() instanceof ObjectType objectColumn) {
+      localizeObjectColumn(entityType, column, objectColumn);
+    } else if (column.getDataType() instanceof ArrayType arrayColumn) {
+      localizeArrayColumn(entityType, column, arrayColumn);
+    }
+  }
+
+  protected void localizeObjectColumn(EntityType entityType, EntityTypeColumn column, ObjectType objectColumn) {
+    objectColumn
+      .getProperties()
+      .forEach(property -> {
+        property.setLabelAlias(
+          getEntityTypeColumnLabelNested(entityType.getName(), column.getName(), property.getName())
+        );
+        if (property.getDataType() instanceof ObjectType nestedObject) {
+          localizeObjectColumn(entityType, column, nestedObject);
+        } else if (property.getDataType() instanceof ArrayType nestedArray) {
+          localizeArrayColumn(entityType, column, nestedArray);
+        }
+      });
+  }
+
+  protected void localizeArrayColumn(EntityType entityType, EntityTypeColumn column, ArrayType arrayColumn) {
+    if (arrayColumn.getItemDataType() instanceof ObjectType nestedObject) {
+      localizeObjectColumn(entityType, column, nestedObject);
+    } else if (arrayColumn.getItemDataType() instanceof ArrayType nestedArray) {
+      localizeArrayColumn(entityType, column, nestedArray);
+    }
   }
 
   public String getEntityTypeLabel(String tableName) {
-    return translationService.format(
-      ENTITY_TYPE_LABEL_TRANSLATION_TEMPLATE.formatted(tableName)
-    );
+    return translationService.format(ENTITY_TYPE_LABEL_TRANSLATION_TEMPLATE.formatted(tableName));
   }
 
   public String getEntityTypeColumnLabel(String tableName, String columnName) {
+    return translationService.format(ENTITY_TYPE_COLUMN_LABEL_TRANSLATION_TEMPLATE.formatted(tableName, columnName));
+  }
+
+  public String getEntityTypeColumnLabelNested(String tableName, String columnName, String nestedPropertyName) {
     return translationService.format(
-      ENTITY_TYPE_COLUMN_LABEL_TRANSLATION_TEMPLATE.formatted(
-        tableName,
-        columnName
-      )
+      ENTITY_TYPE_COLUMN_NESTED_LABEL_TRANSLATION_TEMPLATE.formatted(tableName, columnName, nestedPropertyName)
+    );
+  }
+
+  // Custom field names are already localized as they are user-defined, so we prepend a possessive (e.g. User's)
+  // but leave the {@code customField} untouched
+  public String getEntityTypeCustomFieldLabel(String tableName, String customField) {
+    return translationService.format(
+      ENTITY_TYPE_CUSTOM_FIELD_POSSESSIVE_TRANSLATION_TEMPLATE.formatted(tableName),
+      CUSTOM_FIELD_PARAMETER,
+      customField
     );
   }
 }
--- src/test/java/org/folio/fqm/service/LocalizationServiceTest.java
+++ src/test/java/org/folio/fqm/service/LocalizationServiceTest.java
@@ -6,6 +6,12 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+import org.folio.querytool.domain.dto.ArrayType;
+import org.folio.querytool.domain.dto.EntityType;
+import org.folio.querytool.domain.dto.EntityTypeColumn;
+import org.folio.querytool.domain.dto.NestedObjectProperty;
+import org.folio.querytool.domain.dto.ObjectType;
+import org.folio.querytool.domain.dto.StringType;
 import org.folio.spring.i18n.service.TranslationService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,43 +29,124 @@ class LocalizationServiceTest {
   private LocalizationService localizationService;
 
   @Test
-  void testTableNameFormatting() {
-    String tableName = "table_name";
-    String expectedTranslationKey = "mod-fqm-manager.entityType.table_name";
-    String expectedTranslation = "Table Name";
+  void testBasicEntityTypeFormatting() {
+    String expectedTableTranslationKey = "mod-fqm-manager.entityType.table_name";
+    String expectedColumnTranslationKey = "mod-fqm-manager.entityType.table_name.column_name";
 
-    when(translationService.format(expectedTranslationKey))
-      .thenReturn(expectedTranslation);
+    String expectedTableTranslation = "Table Name";
+    String expectedColumnTranslation = "Column Name";
 
-    String actualTranslation = localizationService.getEntityTypeLabel(
-      tableName
-    );
+    EntityType entityType = new EntityType()
+      .name("table_name")
+      .addColumnsItem(new EntityTypeColumn().name("column_name"));
 
-    assertEquals(expectedTranslation, actualTranslation);
+    when(translationService.format(expectedTableTranslationKey)).thenReturn(expectedTableTranslation);
+    when(translationService.format(expectedColumnTranslationKey)).thenReturn(expectedColumnTranslation);
 
-    verify(translationService, times(1)).format(expectedTranslationKey);
+    EntityType actual = localizationService.localizeEntityType(entityType);
+
+    assertEquals(expectedTableTranslation, actual.getLabelAlias());
+    assertEquals(expectedColumnTranslation, actual.getColumns().get(0).getLabelAlias());
+
+    verify(translationService, times(1)).format(expectedTableTranslationKey);
+    verify(translationService, times(1)).format(expectedColumnTranslationKey);
     verifyNoMoreInteractions(translationService);
   }
 
   @Test
-  void testColumnNameFormatting() {
-    String tableName = "table_name";
-    String columnName = "column_name";
-    String expectedTranslationKey =
-      "mod-fqm-manager.entityType.table_name.column_name";
-    String expectedTranslation = "Column Name";
+  void testCustomFieldFormatting() {
+    String expectedTranslationKey = "mod-fqm-manager.entityType.table_name._custom_field_possessive";
+    String expectedTranslation = "Test's Custom Field";
 
-    when(translationService.format(expectedTranslationKey))
-      .thenReturn(expectedTranslation);
+    EntityType entityType = new EntityType()
+      .name("table_name")
+      .addColumnsItem(new EntityTypeColumn().name("Custom Field").isCustomField(true));
 
-    String actualTranslation = localizationService.getEntityTypeColumnLabel(
-      tableName,
-      columnName
+    when(translationService.format(expectedTranslationKey, "customField", "Custom Field"))
+      .thenReturn("Test's Custom Field");
+
+    localizationService.localizeEntityTypeColumn(entityType, entityType.getColumns().get(0));
+
+    assertEquals(expectedTranslation, entityType.getColumns().get(0).getLabelAlias());
+
+    verify(translationService, times(1)).format(expectedTranslationKey, "customField", "Custom Field");
+    verifyNoMoreInteractions(translationService);
+  }
+
+  @Test
+  void testObjectTypeColumn() {
+    String expectedOuterTranslationKey = "mod-fqm-manager.entityType.table_name.column_name";
+    String expectedOuterTranslation = "Outer Column";
+    String expectedInnerTranslationKey = "mod-fqm-manager.entityType.table_name.column_name.nested_property";
+    String expectedInnerTranslation = "Nested Property";
+
+    EntityType entityType = new EntityType()
+      .name("table_name")
+      .addColumnsItem(
+        new EntityTypeColumn()
+          .name("column_name")
+          .dataType(
+            new ObjectType()
+              .addPropertiesItem(new NestedObjectProperty().dataType(new StringType()).name("nested_property"))
+          )
+      );
+
+    when(translationService.format(expectedOuterTranslationKey)).thenReturn(expectedOuterTranslation);
+    when(translationService.format(expectedInnerTranslationKey)).thenReturn(expectedInnerTranslation);
+
+    localizationService.localizeEntityTypeColumn(entityType, entityType.getColumns().get(0));
+
+    assertEquals(expectedOuterTranslation, entityType.getColumns().get(0).getLabelAlias());
+    assertEquals(
+      expectedInnerTranslation,
+      ((ObjectType) entityType.getColumns().get(0).getDataType()).getProperties().get(0).getLabelAlias()
     );
 
-    assertEquals(expectedTranslation, actualTranslation);
+    verify(translationService, times(1)).format(expectedOuterTranslationKey);
+    verify(translationService, times(1)).format(expectedInnerTranslationKey);
+    verifyNoMoreInteractions(translationService);
+  }
 
-    verify(translationService, times(1)).format(expectedTranslationKey);
+  @Test
+  void testNestedObjectArrayTypeColumn() {
+    String expectedOuterTranslationKey = "mod-fqm-manager.entityType.table_name.column_name";
+    String expectedOuterTranslation = "Outer Column";
+    String expectedInnerTranslationKey = "mod-fqm-manager.entityType.table_name.column_name.nested_property";
+    String expectedInnerTranslation = "Nested Property";
+    String expectedInnermostTranslationKey = "mod-fqm-manager.entityType.table_name.column_name.nested_property_inner";
+    String expectedInnermostTranslation = "Nested * 2 Property";
+
+    // array -> array -> object -> object -> array
+    NestedObjectProperty innermost = new NestedObjectProperty()
+      .dataType(new ArrayType().itemDataType(new StringType()))
+      .name("nested_property_inner");
+    NestedObjectProperty inner = new NestedObjectProperty()
+      .name("nested_property")
+      .dataType(new ObjectType().addPropertiesItem(innermost));
+
+    EntityType entityType = new EntityType()
+      .name("table_name")
+      .addColumnsItem(
+        new EntityTypeColumn()
+          .name("column_name")
+          .dataType(
+            new ArrayType().itemDataType(new ArrayType().itemDataType(new ObjectType().addPropertiesItem(inner)))
+          )
+      );
+
+    when(translationService.format(expectedOuterTranslationKey)).thenReturn(expectedOuterTranslation);
+    when(translationService.format(expectedInnerTranslationKey)).thenReturn(expectedInnerTranslation);
+    when(translationService.format(expectedInnermostTranslationKey)).thenReturn(expectedInnermostTranslation);
+
+    localizationService.localizeEntityTypeColumn(entityType, entityType.getColumns().get(0));
+
+    assertEquals(expectedOuterTranslation, entityType.getColumns().get(0).getLabelAlias());
+    assertEquals(expectedInnerTranslation, inner.getLabelAlias());
+    assertEquals(expectedInnermostTranslation, innermost.getLabelAlias());
+
+    verify(translationService, times(1)).format(expectedOuterTranslationKey);
+    verify(translationService, times(1)).format(expectedInnerTranslationKey);
+    verify(translationService, times(1)).format(expectedInnermostTranslationKey);
     verifyNoMoreInteractions(translationService);
   }
 }

e0147f6ae124b1332553edfaa3bee0d47198ce75
Noah Overcash <novercash@ebsco.com>
2024-02-19 16:03:47 -0500

    Use new Field supertype in SqlFieldIdentificationUtils

--- src/main/java/org/folio/fqm/utils/SqlFieldIdentificationUtils.java
+++ src/main/java/org/folio/fqm/utils/SqlFieldIdentificationUtils.java
@@ -1,8 +1,7 @@
 package org.folio.fqm.utils;
 
 import lombok.experimental.UtilityClass;
-import org.folio.querytool.domain.dto.EntityTypeColumn;
-import org.jooq.Field;
+import org.folio.querytool.domain.dto.Field;
 
 import static org.jooq.impl.DSL.field;
 
@@ -13,30 +12,30 @@ import static org.jooq.impl.DSL.field;
 public class SqlFieldIdentificationUtils {
 
   /**
-   * Returns the version of a SQL field corresponding to an entity type column, intended to be displayed to users
+   * Returns the version of a SQL field corresponding to an entity type field, intended to be displayed to users
    */
-  public static Field<Object> getSqlResultsField(EntityTypeColumn entityTypeColumn) {
-    if (entityTypeColumn.getValueGetter() != null) {
-      return field(entityTypeColumn.getValueGetter());
+  public static org.jooq.Field<Object> getSqlResultsField(Field src) {
+    if (src.getValueGetter() != null) {
+      return field(src.getValueGetter());
     }
     // No value getter is defined for this column. Use column name as the SQL field name.
-    return field(entityTypeColumn.getName());
+    return field(src.getName());
   }
 
   /**
-   * Returns the version of a SQL field corresponding to an entity type column, intended to be used in the WHERE clause.
+   * Returns the version of a SQL field corresponding to an entity type field, intended to be used in the WHERE clause.
    *
    * Note: If both a value getter and a filter value getter is defined for this column, the filter value getter will be used.
    */
-  public static Field<Object> getSqlFilterField(EntityTypeColumn entityTypeColumn) {
-    if (entityTypeColumn.getFilterValueGetter() != null) {
-      return field(entityTypeColumn.getFilterValueGetter());
+  public static org.jooq.Field<Object> getSqlFilterField(Field src) {
+    if (src.getFilterValueGetter() != null) {
+      return field(src.getFilterValueGetter());
     }
-    if (entityTypeColumn.getValueGetter() != null) {
-      return field(entityTypeColumn.getValueGetter());
+    if (src.getValueGetter() != null) {
+      return field(src.getValueGetter());
     } else {
       // No value getter or filter is defined for this column. Use column name as the SQL field name.
-      return field(entityTypeColumn.getName());
+      return field(src.getName());
     }
   }
 }

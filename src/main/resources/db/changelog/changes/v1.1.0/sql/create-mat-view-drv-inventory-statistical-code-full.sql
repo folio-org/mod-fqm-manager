f3782d089c311460a529fcfcf1b350672cf4da85
Noah Overcash <novercash@ebsco.com>
2024-02-20 13:43:55 -0500

    [MODFQMMGR-151] Support nested object queries (#144)
    * Use new Field supertype in SqlFieldIdentificationUtils
    
    * Add valueGetters for POL fields
    
    * Don't cast numbers to string
    
    * Remove extra import
    
    * Extend value getting methods to handle nested properties
    
    * Use Fields for FQL to SQL conversion
    
    * Use correct filter value getters for queries

--- src/main/resources/db/changelog/changes/v1.1.0/sql/create-mat-view-drv-inventory-statistical-code-full.sql
+++ src/main/resources/db/changelog/changes/v1.1.0/sql/create-mat-view-drv-inventory-statistical-code-full.sql
@@ -3,9 +3,9 @@ DROP MATERIALIZED VIEW IF EXISTS drv_inventory_statistical_code_full;
 CREATE MATERIALIZED VIEW drv_inventory_statistical_code_full AS
 SELECT
 	statcode.id AS "id",
-	CONCAT(stattype.jsonb ->> 'name', ': ', statcode.jsonb ->> 'code', ' - ', statcode.jsonb ->> 'name') as "statistical_code"
-FROM corsair_lib_mod_fqm_manager.src_inventory_statistical_code statcode
-JOIN corsair_lib_mod_fqm_manager.src_inventory_statistical_code_type stattype ON stattype.id::text = statcode.jsonb ->> 'statisticalCodeTypeId';
+	CONCAT(stattype.jsonb ->> 'name', ': ', statcode.jsonb ->> 'code', ' - ', statcode.jsonb ->> 'name') AS "statistical_code"
+FROM ${tenant_id}_mod_fqm_manager.src_inventory_statistical_code statcode
+JOIN ${tenant_id}_mod_fqm_manager.src_inventory_statistical_code_type stattype ON stattype.id::text = statcode.jsonb ->> 'statisticalCodeTypeId';
 
 CREATE UNIQUE INDEX fqm_statistical_code_full_idx
 ON ${tenant_id}_mod_fqm_manager.drv_inventory_statistical_code_full(statistical_code);

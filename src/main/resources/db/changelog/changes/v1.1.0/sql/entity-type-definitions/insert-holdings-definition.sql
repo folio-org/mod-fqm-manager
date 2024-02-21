adaf122687188595f968d072238dc487f2682779
Matt Weaver <mweaver@ebsco.com>
2024-02-21 13:22:57 -0500

    MODFQMMGR-108 Add location fields to the Holdings ET

--- src/main/resources/db/changelog/changes/v1.1.0/sql/entity-type-definitions/insert-holdings-definition.sql
+++ src/main/resources/db/changelog/changes/v1.1.0/sql/entity-type-definitions/insert-holdings-definition.sql
@@ -3,9 +3,97 @@ INSERT INTO entity_type_definition (id, derived_table_name, definition)
              "id": "8418e512-feac-4a6a-a56d-9006aab31e33",
              "name":"drv_holdings_record_details",
              "private" : false,
-             "fromClause" : "src_inventory_holdings_record hrd",
+             "fromClause" : "src_inventory_holdings_record hrd JOIN src_inventory_location effective_location ON effective_location.id = hrd.effectivelocationid JOIN src_inventory_loclibrary effective_library ON effective_library.id = effective_location.libraryid JOIN src_inventory_location permanent_location ON permanent_location.id = hrd.permanentlocationid JOIN src_inventory_location temporary_location ON temporary_location.id = hrd.temporarylocationid",
              "columns": [
-                  {
+                 {
+                   "name": "holdings_effective_location",
+                   "dataType": {
+                     "dataType": "stringType"
+                   },
+                   "valueSourceApi": {
+                     "path": "locations",
+                     "valueJsonPath": "$.locations.*.id",
+                     "labelJsonPath": "$.locations.*.name"
+                   },
+                   "source": {
+                     "columnName": "holdings_effective_location",
+                     "entityTypeId": "8418e512-feac-4a6a-a56d-9006aab31e33"
+                   },
+                   "idColumnName": "holdings_effective_location_id",
+                   "valueGetter": "effective_location.jsonb ->> ''name''",
+                   "visibleByDefault": false
+                 },
+                 {
+                   "name": "holdings_effective_location_id",
+                   "dataType": {
+                     "dataType": "rangedUUIDType"
+                   },
+                   "valueSourceApi": {
+                     "path": "locations",
+                     "valueJsonPath": "$.locations.*.id",
+                     "labelJsonPath": "$.locations.*.id"
+                   },
+                   "source": {
+                     "columnName": "holdings_effective_location_id",
+                     "entityTypeId": "8418e512-feac-4a6a-a56d-9006aab31e33"
+                   },
+                   "valueGetter": "hrd.effectivelocationid",
+                   "visibleByDefault": false
+                 },
+                 {
+                   "name": "holdings_effective_library_code",
+                   "dataType": {
+                     "dataType": "stringType"
+                   },
+                   "valueSourceApi": {
+                     "path": "location-units/libraries",
+                     "valueJsonPath": "$.loclibs.*.id",
+                     "labelJsonPath": "$.loclibs.*.code"
+                   },
+                   "source": {
+                     "columnName": "holdings_effective_library_code",
+                     "entityTypeId": "8418e512-feac-4a6a-a56d-9006aab31e33"
+                   },
+                   "idColumnName": "holdings_effective_library_id",
+                   "valueGetter": "effective_library.jsonb ->> ''code''",
+                   "visibleByDefault": false
+                 },
+                 {
+                   "name": "holdings_effective_library_name",
+                   "dataType": {
+                     "dataType": "stringType"
+                   },
+                   "valueSourceApi": {
+                     "path": "location-units/libraries",
+                     "valueJsonPath": "$.loclibs.*.id",
+                     "labelJsonPath": "$.loclibs.*.name"
+                   },
+                   "source": {
+                     "columnName": "holdings_effective_library_name",
+                     "entityTypeId": "8418e512-feac-4a6a-a56d-9006aab31e33"
+                   },
+                   "idColumnName": "holdings_effective_library_id",
+                   "valueGetter": "effective_library.jsonb ->> ''name''",
+                   "visibleByDefault": false
+                 },
+                 {
+                   "name": "holdings_effective_library_id",
+                   "dataType": {
+                     "dataType": "rangedUUIDType"
+                   },
+                   "valueSourceApi": {
+                     "path": "location-units/libraries",
+                     "valueJsonPath": "$.loclibs.*.id",
+                     "labelJsonPath": "$.loclibs.*.id"
+                   },
+                   "source": {
+                     "columnName": "holdings_effective_library_code_id",
+                     "entityTypeId": "8418e512-feac-4a6a-a56d-9006aab31e33"
+                   },
+                   "valueGetter": "effective_library.id",
+                   "visibleByDefault": false
+                 },
+                 {
                      "name": "holdings_hrid",
                      "dataType": {
                          "dataType":"stringType"
@@ -23,6 +111,41 @@ INSERT INTO entity_type_definition (id, derived_table_name, definition)
                      "valueGetter": "hrd.id",
                      "isIdColumn": true,
                      "visibleByDefault": true
+                 },
+                 {
+                     "name": "holdings_permanent_location",
+                     "dataType": {
+                         "dataType": "stringType"
+                     },
+                     "valueSourceApi": {
+                       "path": "locations",
+                       "valueJsonPath": "$.locations.*.id",
+                       "labelJsonPath": "$.locations.*.name"
+                     },
+                     "source": {
+                       "columnName": "holdings_permanent_location",
+                       "entityTypeId": "8418e512-feac-4a6a-a56d-9006aab31e33"
+                     },
+                     "idColumnName": "holdings_permanent_location_id",
+                     "valueGetter": "permanent_location.jsonb ->> ''name''",
+                     "visibleByDefault": false
+                 },
+                 {
+                     "name": "holdings_permanent_location_id",
+                     "dataType": {
+                         "dataType": "rangedUUIDType"
+                     },
+                     "valueSourceApi": {
+                       "path": "locations",
+                       "valueJsonPath": "$.locations.*.id",
+                       "labelJsonPath": "$.locations.*.id"
+                     },
+                     "source": {
+                       "columnName": "holdings_permanent_location_id",
+                       "entityTypeId": "8418e512-feac-4a6a-a56d-9006aab31e33"
+                     },
+                     "valueGetter": "hrd.permanentlocationid",
+                     "visibleByDefault": false
                  },
                  {
                      "name": "holdings_statistical_code_ids",
@@ -70,6 +193,41 @@ INSERT INTO entity_type_definition (id, derived_table_name, definition)
                       "filterValueGetter": "\"left\"(lower(hrd.jsonb ->> ''discoverySuppress''::text), 600)",
                       "valueFunction": "\"left\"(lower(:value), 600)",
                       "visibleByDefault": true
+                  },
+                  {
+                    "name": "holdings_temporary_location",
+                    "dataType": {
+                      "dataType": "stringType"
+                    },
+                    "valueSourceApi": {
+                      "path": "locations",
+                      "valueJsonPath": "$.locations.*.id",
+                      "labelJsonPath": "$.locations.*.name"
+                    },
+                    "source": {
+                      "columnName": "holdings_temporary_location",
+                      "entityTypeId": "8418e512-feac-4a6a-a56d-9006aab31e33"
+                    },
+                    "idColumnName": "holdings_temporary_location_id",
+                    "valueGetter": "temporary_location.jsonb ->> ''name''",
+                    "visibleByDefault": false
+                  },
+                  {
+                    "name": "holdings_temporary_location_id",
+                    "dataType": {
+                      "dataType": "rangedUUIDType"
+                    },
+                    "valueSourceApi": {
+                      "path": "locations",
+                      "valueJsonPath": "$.locations.*.id",
+                      "labelJsonPath": "$.locations.*.id"
+                    },
+                    "source": {
+                      "columnName": "holdings_temporary_location_id",
+                      "entityTypeId": "8418e512-feac-4a6a-a56d-9006aab31e33"
+                    },
+                    "valueGetter": "hrd.temporarylocationid",
+                    "visibleByDefault": false
                   },
                   {
                        "name": "instance_id",

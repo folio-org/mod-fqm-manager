6efc98d713f45228a98509121780616aabd0553a
Noah Overcash <novercash@ebsco.com>
2024-03-05 15:02:07 -0500

    [MODFQMMGR-141] Add queryable flag (#163)

--- src/main/resources/db/changelog/changes/v1.1.0/sql/entity-type-definitions/insert-organization-definition.sql
+++ src/main/resources/db/changelog/changes/v1.1.0/sql/entity-type-definitions/insert-organization-definition.sql
@@ -1,15 +1,16 @@
 INSERT INTO entity_type_definition (id, derived_table_name, definition)
     VALUES ('837f262e-2073-4a00-8bcc-4e4ce6e669b3','drv_organization_details', '{
-             "id": "837f262e-2073-4a00-8bcc-4e4ce6e669b3",
-             "name":"drv_organization_details",
-             "private" : false,
-             "fromClause" : "src_organizations as org",
-             "columns" : [
+              "id": "837f262e-2073-4a00-8bcc-4e4ce6e669b3",
+              "name": "drv_organization_details",
+              "private": false,
+              "fromClause": "src_organizations as org",
+              "columns": [
                 {
                   "name": "last_updated",
                   "dataType": {
                     "dataType": "dateType"
                   },
+                  "queryable": true,
                   "valueGetter": "org.jsonb->''metadata''->>''updatedDate''",
                   "visibleByDefault": false
                 },
@@ -18,6 +19,7 @@ INSERT INTO entity_type_definition (id, derived_table_name, definition)
                   "dataType": {
                     "dataType": "stringType"
                   },
+                  "queryable": true,
                   "valueGetter": "org.jsonb->>''code''",
                   "filterValueGetter": "lower(${tenant_id}_mod_organizations_storage.f_unaccent(org.jsonb->>''code''::text))",
                   "valueFunction": "lower(${tenant_id}_mod_organizations_storage.f_unaccent(:value))",
@@ -28,6 +30,7 @@ INSERT INTO entity_type_definition (id, derived_table_name, definition)
                   "dataType": {
                     "dataType": "stringType"
                   },
+                  "queryable": true,
                   "valueGetter": "org.jsonb->>''name''",
                   "filterValueGetter": "lower(${tenant_id}_mod_organizations_storage.f_unaccent(org.jsonb->>''name''::text))",
                   "valueFunction": "lower(${tenant_id}_mod_organizations_storage.f_unaccent(:value))",
@@ -52,6 +55,7 @@ INSERT INTO entity_type_definition (id, derived_table_name, definition)
                   "dataType": {
                     "dataType": "stringType"
                   },
+                  "queryable": true,
                   "valueGetter": "org.jsonb->>''status''",
                   "filterValueGetter": "lower(${tenant_id}_mod_organizations_storage.f_unaccent(org.jsonb->>''status''::text))",
                   "valueFunction": "lower(${tenant_id}_mod_organizations_storage.f_unaccent(:value))",
@@ -62,6 +66,7 @@ INSERT INTO entity_type_definition (id, derived_table_name, definition)
                   "dataType": {
                     "dataType": "stringType"
                   },
+                  "queryable": true,
                   "valueGetter": "org.jsonb->>''description''",
                   "visibleByDefault": false
                 },
@@ -70,6 +75,7 @@ INSERT INTO entity_type_definition (id, derived_table_name, definition)
                   "dataType": {
                     "dataType": "stringType"
                   },
+                  "queryable": true,
                   "valueGetter": "org.jsonb->>''erpCode''",
                   "filterValueGetter": "lower(${tenant_id}_mod_organizations_storage.f_unaccent(org.jsonb->>''erpCode''::text))",
                   "valueFunction": "lower(${tenant_id}_mod_organizations_storage.f_unaccent(:value))",
@@ -83,6 +89,7 @@ INSERT INTO entity_type_definition (id, derived_table_name, definition)
                       "dataType": "stringType"
                     }
                   },
+                  "queryable": true,
                   "valueGetter": "(SELECT array_agg(alias) FROM jsonb_array_elements_text(org.jsonb -> ''aliases'') AS alias)",
                   "filterValueGetter": "lower(${tenant_id}_mod_organizations_storage.f_unaccent(org.jsonb->>''aliases''::text))",
                   "valueFunction": "lower(${tenant_id}_mod_organizations_storage.f_unaccent(:value))",
@@ -96,6 +103,7 @@ INSERT INTO entity_type_definition (id, derived_table_name, definition)
                       "dataType": "rangedUUIDType"
                     }
                   },
+                  "queryable": true,
                   "valueGetter": "( SELECT array_agg(record.value::text) FILTER (WHERE (record.value::text) IS NOT NULL) AS array_agg FROM jsonb_array_elements_text(org.jsonb -> ''organizationTypes''::text) record(value))",
                   "visibleByDefault": false
                 },
@@ -107,19 +115,21 @@ INSERT INTO entity_type_definition (id, derived_table_name, definition)
                       "dataType": "stringType"
                     }
                   },
+                  "queryable": true,
                   "idColumnName": "organization_type_ids",
                   "source": {
                     "entityTypeId": "6b335e41-2654-4e2a-9b4e-c6930b330ccc",
                     "columnName": "organization_types_name"
                   },
-                   "valueGetter": "( SELECT array_agg(a.jsonb ->> ''name''::text) FILTER (WHERE (a.jsonb ->> ''name''::text) IS NOT NULL) AS array_agg FROM jsonb_array_elements_text((org.jsonb -> ''organizationTypes''::text)) record(value) JOIN src_organization_types a ON (record.value::text) = a.id::text)",
-                   "visibleByDefault": false
+                  "valueGetter": "( SELECT array_agg(a.jsonb ->> ''name''::text) FILTER (WHERE (a.jsonb ->> ''name''::text) IS NOT NULL) AS array_agg FROM jsonb_array_elements_text((org.jsonb -> ''organizationTypes''::text)) record(value) JOIN src_organization_types a ON (record.value::text) = a.id::text)",
+                  "visibleByDefault": false
                 },
                 {
                   "name": "id",
                   "dataType": {
                     "dataType": "rangedUUIDType"
                   },
+                  "queryable": true,
                   "valueGetter": "org.id",
                   "isIdColumn": true,
                   "visibleByDefault": true
@@ -151,14 +161,16 @@ INSERT INTO entity_type_definition (id, derived_table_name, definition)
                   "dataType": {
                     "dataType": "stringType"
                   },
+                  "queryable": true,
                   "valueGetter": "org.jsonb->>''paymentMethod''",
                   "visibleByDefault": false
                 },
-               {
+                {
                   "name": "discount_percent",
                   "dataType": {
                     "dataType": "numberType"
                   },
+                  "queryable": true,
                   "valueGetter": "org.jsonb->>''discountPercent''",
                   "visibleByDefault": false
                 },
@@ -167,97 +179,107 @@ INSERT INTO entity_type_definition (id, derived_table_name, definition)
                   "dataType": {
                     "dataType": "integerType"
                   },
+                  "queryable": true,
                   "valueGetter": "org.jsonb->>''claimingInterval''",
                   "visibleByDefault": false
                 },
                 {
-                   "name": "export_to_accounting",
-                   "values": [
-                     {
-                       "label": "True",
-                       "value": "true"
-                     },
-                     {
-                       "label": "False",
-                       "value": "false"
-                     }
-                   ],
-                   "dataType": {
-                     "dataType": "booleanType"
-                   },
-                   "valueGetter": "org.jsonb->>''exportToAccounting''",
-                   "visibleByDefault": false
-                 },
-                 {
-                   "name": "subscription_interval",
-                   "dataType": {
-                     "dataType": "integerType"
-                   },
-                   "valueGetter": "org.jsonb->>''subscriptionInterval''",
-                   "visibleByDefault": false
-                 },
-                 {
-                   "name": "expected_invoice_interval",
-                   "dataType": {
-                     "dataType": "integerType"
-                   },
-                   "valueGetter": "org.jsonb->>''expectedInvoiceInterval''",
-                   "visibleByDefault": false
-                 },
-                 {
-                   "name": "expected_receipt_interval",
-                   "dataType": {
-                     "dataType": "integerType"
-                   },
-                   "valueGetter": "org.jsonb->>''expectedReceiptInterval''",
-                   "visibleByDefault": false
-                 },
-                 {
-                   "name": "renewal_activation_interval",
-                   "dataType": {
-                     "dataType": "integerType"
-                   },
-                   "valueGetter": "org.jsonb->>''renewalActivationInterval''",
-                   "visibleByDefault": false
-                 },
-                 {
-                   "name": "expected_activation_interval",
-                   "dataType": {
-                     "dataType": "integerType"
-                   },
-                   "valueGetter": "org.jsonb->>''expectedActivationInterval''",
-                   "visibleByDefault": false
-                 },
-                 {
-                    "name": "tax_id",
-                    "dataType": {
+                  "name": "export_to_accounting",
+                  "values": [
+                    {
+                      "label": "True",
+                      "value": "true"
+                    },
+                    {
+                      "label": "False",
+                      "value": "false"
+                    }
+                  ],
+                  "dataType": {
+                    "dataType": "booleanType"
+                  },
+                  "queryable": true,
+                  "valueGetter": "org.jsonb->>''exportToAccounting''",
+                  "visibleByDefault": false
+                },
+                {
+                  "name": "subscription_interval",
+                  "dataType": {
+                    "dataType": "integerType"
+                  },
+                  "queryable": true,
+                  "valueGetter": "org.jsonb->>''subscriptionInterval''",
+                  "visibleByDefault": false
+                },
+                {
+                  "name": "expected_invoice_interval",
+                  "dataType": {
+                    "dataType": "integerType"
+                  },
+                  "queryable": true,
+                  "valueGetter": "org.jsonb->>''expectedInvoiceInterval''",
+                  "visibleByDefault": false
+                },
+                {
+                  "name": "expected_receipt_interval",
+                  "dataType": {
+                    "dataType": "integerType"
+                  },
+                  "queryable": true,
+                  "valueGetter": "org.jsonb->>''expectedReceiptInterval''",
+                  "visibleByDefault": false
+                },
+                {
+                  "name": "renewal_activation_interval",
+                  "dataType": {
+                    "dataType": "integerType"
+                  },
+                  "queryable": true,
+                  "valueGetter": "org.jsonb->>''renewalActivationInterval''",
+                  "visibleByDefault": false
+                },
+                {
+                  "name": "expected_activation_interval",
+                  "dataType": {
+                    "dataType": "integerType"
+                  },
+                  "queryable": true,
+                  "valueGetter": "org.jsonb->>''expectedActivationInterval''",
+                  "visibleByDefault": false
+                },
+                {
+                  "name": "tax_id",
+                  "dataType": {
+                    "dataType": "stringType"
+                  },
+                  "queryable": true,
+                  "valueGetter": "org.jsonb->>''taxId''",
+                  "filterValueGetter": "lower(${tenant_id}_mod_organizations_storage.f_unaccent(org.jsonb ->> ''taxId''::text))",
+                  "valueFunction": "lower(${tenant_id}_mod_organizations_storage.f_unaccent(:value))",
+                  "visibleByDefault": false
+                },
+                {
+                  "name": "tax_percentage",
+                  "dataType": {
+                    "dataType": "numberType"
+                  },
+                  "queryable": true,
+                  "valueGetter": "org.jsonb->>''taxPercentage''",
+                  "visibleByDefault": false
+                },
+                {
+                  "name": "vendor_currencies",
+                  "dataType": {
+                    "dataType": "arrayType",
+                    "itemDataType": {
                       "dataType": "stringType"
-                    },
-                    "valueGetter": "org.jsonb->>''taxId''",
-                    "filterValueGetter": "lower(${tenant_id}_mod_organizations_storage.f_unaccent(org.jsonb ->> ''taxId''::text))",
-                    "valueFunction": "lower(${tenant_id}_mod_organizations_storage.f_unaccent(:value))",
-                    "visibleByDefault": false
+                    }
                   },
-                  {
-                    "name": "tax_percentage",
-                    "dataType": {
-                      "dataType": "numberType"
-                    },
-                    "valueGetter": "org.jsonb->>''taxPercentage''",
-                    "visibleByDefault": false
-                  },
-                 {
-                   "name": "vendor_currencies",
-                   "dataType": {
-                     "dataType": "arrayType",
-                     "itemDataType": {
-                       "dataType": "stringType"
-                     }
-                   },
-                   "valueGetter": "(SELECT array_agg(vendorCurrency) FROM jsonb_array_elements_text(org.jsonb->''vendorCurrencies'') AS vendorCurrency)",
-                   "visibleByDefault": false
-                 },
-                 {
+                  "queryable": true,
+                  "valueGetter": "(SELECT array_agg(vendorCurrency) FROM jsonb_array_elements_text(org.jsonb->''vendorCurrencies'') AS vendorCurrency)",
+                  "visibleByDefault": false
+                },
+                {
                   "name": "agreements",
                   "dataType": {
                     "dataType": "arrayType",
@@ -267,44 +289,56 @@ INSERT INTO entity_type_definition (id, derived_table_name, definition)
                         {
                           "name": "name",
                           "property": "name",
-                          "dataType": {"dataType": "stringType"},
+                          "dataType": {
+                            "dataType": "stringType"
+                          },
+                          "queryable": true,
                           "valueGetter": "( SELECT array_agg(elems.value ->> ''name'') FROM jsonb_array_elements(org.jsonb -> ''agreements'') AS elems)",
                           "filterValueGetter": "( SELECT array_agg(lower(elems.value ->> ''name'')) FROM jsonb_array_elements(org.jsonb -> ''agreements'') AS elems)",
                           "valueFunction": "lower(:value)"
                         },
-                       {
+                        {
                           "name": "discount",
                           "property": "discount",
-                          "dataType": {"dataType": "numberType"},
+                          "dataType": {
+                            "dataType": "numberType"
+                          },
+                          "queryable": true,
                           "valueGetter": "( SELECT array_agg(elems.value ->> ''discount'') FROM jsonb_array_elements(org.jsonb -> ''agreements'') AS elems)"
                         },
                         {
                           "name": "referenceUrl",
                           "property": "referenceUrl",
-                          "dataType": {"dataType": "stringType"},
+                          "dataType": {
+                            "dataType": "stringType"
+                          },
+                          "queryable": true,
                           "valueGetter": "( SELECT array_agg(elems.value ->> ''referenceUrl'') FROM jsonb_array_elements(org.jsonb -> ''agreements'') AS elems)",
                           "filterValueGetter": "( SELECT array_agg(lower(elems.value ->> ''referenceUrl'')) FROM jsonb_array_elements(org.jsonb -> ''agreements'') AS elems)",
                           "valueFunction": "lower(:value)"
                         },
                         {
-                           "name": "notes",
-                           "property": "notes",
-                           "dataType": {"dataType": "stringType"},
-                           "valueGetter": "( SELECT array_agg(elems.value ->> ''notes'') FROM jsonb_array_elements(org.jsonb -> ''agreements'') AS elems)",
-                           "filterValueGetter": "( SELECT array_agg(lower(elems.value ->> ''notes'')) FROM jsonb_array_elements(org.jsonb -> ''agreements'') AS elems)",
-                           "valueFunction": "lower(:value)"
+                          "name": "notes",
+                          "property": "notes",
+                          "dataType": {
+                            "dataType": "stringType"
+                          },
+                          "queryable": true,
+                          "valueGetter": "( SELECT array_agg(elems.value ->> ''notes'') FROM jsonb_array_elements(org.jsonb -> ''agreements'') AS elems)",
+                          "filterValueGetter": "( SELECT array_agg(lower(elems.value ->> ''notes'')) FROM jsonb_array_elements(org.jsonb -> ''agreements'') AS elems)",
+                          "valueFunction": "lower(:value)"
                         }
                       ]
                     }
-                   },
+                  },
                   "valueGetter": "org.jsonb ->> ''agreements''",
                   "visibleByDefault": false
                 }
               ],
-             "defaultSort": [
-               {
-                   "columnName": "id",
-                   "direction": "ASC"
-               }
-             ]
-         }') ON CONFLICT (id) DO UPDATE SET derived_table_name = EXCLUDED.derived_table_name, definition = EXCLUDED.definition;
+              "defaultSort": [
+                {
+                  "columnName": "id",
+                  "direction": "ASC"
+                }
+              ]
+            }') ON CONFLICT (id) DO UPDATE SET derived_table_name = EXCLUDED.derived_table_name, definition = EXCLUDED.definition;

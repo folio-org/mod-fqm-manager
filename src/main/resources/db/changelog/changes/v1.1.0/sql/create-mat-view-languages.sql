7540668295e04367949195b8670f3a0b2e1b5512
Kriti Jain <kjain@ebsco.com>
2024-03-11 12:58:54 -0400

    missing-s: fixing deployment failure cz of missing s

--- src/main/resources/db/changelog/changes/v1.1.0/sql/create-mat-view-languages.sql
+++ src/main/resources/db/changelog/changes/v1.1.0/sql/create-mat-view-languages.sql
@@ -1,7 +1,7 @@
 DROP MATERIALIZED VIEW IF EXISTS drv_languages;
 
  CREATE MATERIALIZED VIEW drv_languages AS
- SELECT DISTINCT elements.language FROM ${tenant_id}_mod_inventory_storage.instance, jsonb_array_elements_text(jsonb -> 'languages') AS elements(language);
+ SELECT DISTINCT elements.languages FROM ${tenant_id}_mod_inventory_storage.instance, jsonb_array_elements_text(jsonb -> 'languages') AS elements(languages);
 
  CREATE UNIQUE INDEX fqm_languages_idx
  ON ${tenant_id}_mod_fqm_manager.drv_languages(languages);

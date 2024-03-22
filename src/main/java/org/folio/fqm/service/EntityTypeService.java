8081049e7be898eb0e7bdedd53f275eb00115485
Kriti Jain <kjain@ebsco.com>
2024-03-22 09:25:56 -0400

    fixing-alpha:fixing stupid mistake

--- src/main/java/org/folio/fqm/service/EntityTypeService.java
+++ src/main/java/org/folio/fqm/service/EntityTypeService.java
@@ -187,7 +187,7 @@ public class EntityTypeService {
   }
   private void sortColumnsInEntityType(EntityType entityType) {
     List<EntityTypeColumn> sortedColumns = entityType.getColumns().stream()
-      .sorted(Comparator.comparing(EntityTypeColumn::getName))
+      .sorted(Comparator.comparing(EntityTypeColumn::getLabelAlias))
       .collect(Collectors.toList());
     entityType.setColumns(sortedColumns);
   }

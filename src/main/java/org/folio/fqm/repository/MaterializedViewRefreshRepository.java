d7c3d96dec32b4ab633043e4b5214a02a9dec61f
Bobby Sharp <bsharp@ebsco.com>
2024-03-26 10:42:53 -0400

    Create currency-exchange-rates table

--- src/main/java/org/folio/fqm/repository/MaterializedViewRefreshRepository.java
+++ src/main/java/org/folio/fqm/repository/MaterializedViewRefreshRepository.java
@@ -2,6 +2,7 @@ package org.folio.fqm.repository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.folio.fqm.client.SimpleHttpClient;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @Log4j2
 public class MaterializedViewRefreshRepository {
+
   private static final String REFRESH_MATERIALIZED_VIEW_SQL = "REFRESH MATERIALIZED VIEW CONCURRENTLY ";
 
   private static final List<String> materializedViewNames = List.of(
@@ -22,13 +24,31 @@ public class MaterializedViewRefreshRepository {
     "drv_languages"
   );
 
+  private static final List<String> SYSTEM_SUPPORTED_CURRENCIES = List.of(
+    "USD",
+    "GBP"
+  );
+
   private final DSLContext jooqContext;
 
+  private final SimpleHttpClient simpleHttpClient;
+
   public void refreshMaterializedViews(String tenantId) {
-    for (String matViewName : materializedViewNames) {
-      String fullName = tenantId + "_mod_fqm_manager." + matViewName;
-      log.info("Refreshing materialized view {}", fullName);
-      jooqContext.execute(REFRESH_MATERIALIZED_VIEW_SQL + fullName);
+//    for (String matViewName : materializedViewNames) {
+//      String fullName = tenantId + "_mod_fqm_manager." + matViewName;
+//      log.info("Refreshing materialized view {}", fullName);
+//      jooqContext.execute(REFRESH_MATERIALIZED_VIEW_SQL + fullName);
+//    }
+    refreshExchangeRates(tenantId);
+  }
+
+  public void refreshExchangeRates(String tenantId) {
+    String localeSettingsGetter = "configurations/entries?query=(module==ORG and configName==localeSettings)";
+    log.info("Refreshing exchange rates");
+    String defaultCurrencyCode = "USD"; // TODO: have to get this from somewhere
+    for (String currencyCode : SYSTEM_SUPPORTED_CURRENCIES) {
+      log.info("Getting currency exchange rate from {} to {}", defaultCurrencyCode, currencyCode);
+
     }
   }
 }
---
+++ src/main/resources/db/changelog/changes/v1.1.1/sql/create-table-currency-exchange-rates.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS currency_exchange_rates(
+  currency VARCHAR(255),
+  exchange_rate FLOAT
+);

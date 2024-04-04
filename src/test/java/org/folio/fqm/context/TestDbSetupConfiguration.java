5b1458958304f0373770ec3d3a85e7ce2c55bb7b
Noah Overcash <novercash@ebsco.com>
2024-04-04 11:45:47 -0400

    bean configs

--- src/test/java/org/folio/fqm/context/TestDbSetupConfiguration.java
+++ src/test/java/org/folio/fqm/context/TestDbSetupConfiguration.java
@@ -4,12 +4,15 @@ import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 import javax.sql.DataSource;
 import liquibase.integration.spring.SpringLiquibase;
+import org.folio.fqm.repository.EntityTypeRepository;
 import org.folio.fqm.service.EntityTypeInitializationService;
+import org.folio.spring.FolioExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 /**
  * This class is responsible for inserting test data into a PostgreSQL test container database.
@@ -17,6 +20,12 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("db-test")
 public class TestDbSetupConfiguration {
+
+  @Autowired
+  EntityTypeRepository entityTypeRepository;
+
+  @Autowired
+  ResourcePatternResolver resourceResolver;
 
   @Bean
   SpringLiquibase springLiquibase(DataSource dataSource) {
@@ -30,13 +39,26 @@ public class TestDbSetupConfiguration {
   @Bean
   @DependsOn("readerDataSource")
   EntityTypeInitializer entityTypeInitializer() {
-    return new EntityTypeInitializer();
+    return new EntityTypeInitializer(entityTypeRepository, resourceResolver);
   }
 
   static class EntityTypeInitializer {
 
-    @Autowired
     private EntityTypeInitializationService entityTypeInitializationService;
+
+    public EntityTypeInitializer(EntityTypeRepository entityTypeRepository, ResourcePatternResolver resourceResolver) {
+      // I didn't want to provide a bean of FolioExecutionContext, in case it'd conflict with other tests
+      this.entityTypeInitializationService =
+        new EntityTypeInitializationService(
+          entityTypeRepository,
+          new FolioExecutionContext() {
+            public String getTenantId() {
+              return "beeuni";
+            }
+          },
+          resourceResolver
+        );
+    }
 
     @PostConstruct
     public void populateEntityTypes() throws IOException {

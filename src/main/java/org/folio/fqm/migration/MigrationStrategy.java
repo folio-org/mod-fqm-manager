83124877726116eb1d473b6ccbfcb1fbaa3bbaab
Noah Overcash <novercash@ebsco.com>
2024-07-22 20:46:32 -0400

    docblock

--- src/main/java/org/folio/fqm/migration/MigrationStrategy.java
+++ src/main/java/org/folio/fqm/migration/MigrationStrategy.java
@@ -7,5 +7,15 @@ public interface MigrationStrategy
   extends BiFunction<FqlService, MigratableQueryInformation, MigratableQueryInformation> {
   /** For logging purposes */
   String getLabel();
+
   boolean applies(FqlService fqlService, MigratableQueryInformation migratableQueryInformation);
+
+  // respecified to add docblock
+  /**
+   * Migrate the query. This method will be called iff {@link #applies(FqlService, MigratableQueryInformation)} returns true.
+   *
+   * After this method is called, {@link #applies(FqlService, MigratableQueryInformation)} MUST return false. Otherwise, an infinite
+   * loop will occur.
+   */
+  MigratableQueryInformation apply(FqlService fqlService, MigratableQueryInformation migratableQueryInformation);
 }

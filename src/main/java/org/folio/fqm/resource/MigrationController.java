d9eb2312e01c590e26855e868c5811e723b0c446
Kriti Jain <kjain@ebsco.com>
2024-08-13 11:52:50 -0500

    re

--- src/main/java/org/folio/fqm/resource/MigrationController.java
+++ src/main/java/org/folio/fqm/resource/MigrationController.java
@@ -1,6 +1,7 @@
 package org.folio.fqm.resource;
 
 import lombok.RequiredArgsConstructor;
+import org.folio.fqm.migration.MigratableQueryInformation;
 import org.folio.fqm.service.MigrationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MigrationController implements FqmVersionApi {
   private final MigrationService migrationService;
-
+  private final MigratableQueryInformation migratableQueryInformation;
   @Override
   public ResponseEntity<String> getFqmVersion() {
     return new ResponseEntity<>(migrationService.getLatestVersion(), HttpStatus.OK);
+  }
+  @Override
+  public ResponseEntity<MigratableQueryInformation> fqmMigrate() {
+    return ResponseEntity.ok(migrationService.migrate(migratableQueryInformation));
   }
 }

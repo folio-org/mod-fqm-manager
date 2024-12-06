0bbed034bb900c934fb776c263e395f6d59a0c77
Bobby Sharp <bsharp@ebsco.com>
2024-12-06 08:21:28 -0500

    Fix application.yml (again)

--- src/main/java/org/folio/fqm/config/ScheduledExecutorServiceConfig.java
+++ src/main/java/org/folio/fqm/config/ScheduledExecutorServiceConfig.java
@@ -1,6 +1,5 @@
 package org.folio.fqm.config;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +7,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 @Configuration
-@Log4j2
 public class ScheduledExecutorServiceConfig {
   @Bean(destroyMethod = "shutdown")
   public ScheduledExecutorService scheduledExecutorService() {

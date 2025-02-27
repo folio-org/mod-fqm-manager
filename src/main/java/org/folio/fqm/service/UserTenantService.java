a82258b840b2cac19e0300dfd04f5738920a219a
Noah Overcash <novercash@ebsco.com>
2025-02-27 15:12:13 -0500

    oops

--- src/main/java/org/folio/fqm/service/UserTenantService.java
+++ src/main/java/org/folio/fqm/service/UserTenantService.java
@@ -1,11 +1,12 @@
 package org.folio.fqm.service;
 
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.fqm.client.SimpleHttpClient;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 /**
  * Service wrapper for caching responses from user-tenants API.
@@ -17,8 +18,9 @@ public class UserTenantService {
 
   private final SimpleHttpClient userTenantsClient;
 
-  @Cacheable(value = "userTenantCache", key = "#tenantId")
+  @Cacheable(value="userTenantCache", key="#tenantId")
   public String getUserTenantsResponse(String tenantId) {
-    return "{\"userTenants\": [], \"totalRecords\": 0}";
+    log.info("Retrieving user-tenants information for tenant {}", tenantId);
+    return userTenantsClient.get("user-tenants", Map.of("limit", String.valueOf(1)));
   }
 }

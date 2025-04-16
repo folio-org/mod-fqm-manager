0c7c499d851112502fec095a84a21c5aec383664
Matt Weaver <mweaver@ebsco.com>
2025-04-16 10:54:08 -0700

    MODFQMMGR-518 Add retry when looking for zombie queries 🧟

--- src/test/java/org/folio/fqm/repository/QueryRepositoryTest.java
+++ src/test/java/org/folio/fqm/repository/QueryRepositoryTest.java
@@ -3,6 +3,7 @@ package org.folio.fqm.repository;
 import org.folio.fqm.domain.Query;
 import org.folio.fqm.domain.QueryStatus;
 import org.folio.querytool.domain.dto.QueryIdentifier;
+import org.jooq.DSLContext;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,9 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 import static org.springframework.util.StringUtils.hasText;
 
 @ActiveProfiles("db-test")
@@ -23,13 +27,15 @@ import static org.springframework.util.StringUtils.hasText;
 class QueryRepositoryTest {
 
   @Autowired
-  private QueryRepository repo;
+  DSLContext jooqContext;
 
+  private QueryRepository repo;
   private UUID queryId;
 
   @BeforeEach
   public void setUp() {
     queryId = UUID.randomUUID();
+    repo = spy(new QueryRepository(jooqContext, jooqContext, 0));
   }
 
   @AfterEach
@@ -49,7 +55,7 @@ class QueryRepositoryTest {
     Query expectedQuery = new Query(queryId, entityTypeId, fqlQuery, fields,
       createdBy, OffsetDateTime.now(), null, QueryStatus.IN_PROGRESS, null);
     QueryIdentifier queryIdentifier = repo.saveQuery(expectedQuery);
-    Query actualQuery = repo.getQuery(queryIdentifier.getQueryId()).orElse(null);
+    Query actualQuery = repo.getQuery(queryIdentifier.getQueryId());
     assertEquals(expectedQuery.queryId(), actualQuery.queryId());
     assertEquals(expectedQuery.entityTypeId(), actualQuery.entityTypeId());
     assertEquals(expectedQuery.createdBy(), actualQuery.createdBy());
@@ -142,6 +148,26 @@ class QueryRepositoryTest {
     actual = repo.getQuery(queryIdentifier.getQueryId(), false).orElseThrow(() -> new RuntimeException("Query not found"));
     // Then it should still be marked as failed, since the first getQuery() call changed its status
     assertEquals(QueryStatus.FAILED, actual.status());
+  }
+
+  @Test
+  void shouldNotFailQueriesThatAreStillRunning() {
+    UUID entityTypeId = UUID.randomUUID();
+    UUID createdBy = UUID.randomUUID();
+    String fqlQuery = """
+      {"field1": {"$in": ["value1", "value2", "value3", "value4", "value5" ] }}
+      """;
+    List<String> fields = List.of("id", "field1", "field2");
+
+    Query expectedQuery = new Query(queryId, entityTypeId, fqlQuery, fields,
+      createdBy, OffsetDateTime.now(), null, QueryStatus.IN_PROGRESS, null);
+    // Given a query which is saved with the in-progress status and has a running SQL query backing it
+    QueryIdentifier queryIdentifier = repo.saveQuery(expectedQuery);
+    when(repo.getQueryPids(eq(queryId))).thenReturn(List.of(123));
+    // When you retrieve it with getQuery(id, false)
+    Query actual = repo.getQuery(queryIdentifier.getQueryId(), false).orElseThrow(() -> new RuntimeException("Query not found"));
+    // Then it should still be in-progress, since the getQuery() call didn't change its status'
+    assertEquals(QueryStatus.IN_PROGRESS, actual.status());
   }
 
   @Test

adaf05715fd51327d0dcc21be9a35c67c89be059
Matt Weaver <mweaver@ebsco.com>
2025-05-15 12:28:35 -0600

    MODFQMMGR-632 Update the custom ET deletion endpoint to prevent deleting non-custom ETs

--- src/main/java/org/folio/fqm/exception/EntityTypeNotFoundException.java
+++ src/main/java/org/folio/fqm/exception/EntityTypeNotFoundException.java
@@ -1,19 +1,27 @@
 package org.folio.fqm.exception;
 
-import lombok.RequiredArgsConstructor;
 import org.folio.fqm.domain.dto.Error;
 import org.folio.fqm.domain.dto.Parameter;
 import org.springframework.http.HttpStatus;
 
 import java.util.UUID;
 
-@RequiredArgsConstructor
 public class EntityTypeNotFoundException extends FqmException {
   private final UUID entityTypeId;
+  private final String message;
+
+  public EntityTypeNotFoundException(UUID entityTypeId, String message) {
+    this.entityTypeId = entityTypeId;
+    this.message = message;
+  }
+
+  public EntityTypeNotFoundException(UUID entityTypeId) {
+    this(entityTypeId, "Entity type with ID " + entityTypeId + " not found");
+  }
 
   @Override
   public String getMessage() {
-    return "Entity type with ID " + entityTypeId + " not found";
+    return message;
   }
 
   @Override

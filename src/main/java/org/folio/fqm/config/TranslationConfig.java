c955132db9353fccdb6bf78d811f1035bc8a2e63
Noah Overcash <novercash@ebsco.com>
2025-06-12 17:13:49 -0400

    primaryify

--- src/main/java/org/folio/fqm/config/TranslationConfig.java
+++ src/main/java/org/folio/fqm/config/TranslationConfig.java
@@ -5,11 +5,13 @@ import java.util.Locale;
 import org.folio.spring.i18n.config.TranslationConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class TranslationConfig {
 
   @Bean
+  @Primary
   public TranslationConfiguration translationConfiguration() {
     return new TranslationConfiguration(List.of("/translations/", "/external-translations/"), Locale.ENGLISH);
   }

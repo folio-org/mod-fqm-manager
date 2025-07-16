20ac0714f770517963dc1fab0d7ecd871cc79183
bvsharp <97990858+bvsharp@users.noreply.github.com>
2025-07-16 18:00:36 -0400

    [MODFQMMGR-679] Include 'created by' and 'updated by' as essential fields on several record types (#829)

---
+++ src/main/java/org/folio/fqm/migration/strategies/V15OrganizationSimpleToCompositeMigration.java
@@ -0,0 +1,45 @@
+package org.folio.fqm.migration.strategies;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.folio.fqm.migration.AbstractSimpleMigrationStrategy;
+
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Version 15 -> 16, migrates from simple_organization to composite_organization entity type.
+ * The public Organizations ET is being changed from a simple ET to a composite. Because of this,
+ * all fields get "organization." prepended to their names.
+ * @see https://folio-org.atlassian.net/browse/MODFQMMGR-679
+ */
+@Log4j2
+@RequiredArgsConstructor
+public class V15OrganizationSimpleToCompositeMigration extends AbstractSimpleMigrationStrategy {
+
+  private static final UUID SIMPLE_ORGANIZATIONS_ID = UUID.fromString("b5ffa2e9-8080-471a-8003-a8c5a1274503");
+  private static final UUID COMPOSITE_ORGANIZATIONS_ID = UUID.fromString("e0ea4212-4023-458a-adce-8003ff6c5d9e");
+
+  @Override
+  public String getMaximumApplicableVersion() {
+    return "15";
+  }
+
+  @Override
+  public String getLabel() {
+    return "V15 -> V16 Organization simple to composite migration (part of MODFQMMGR-679)";
+  }
+
+  @Override
+  public Map<UUID, UUID> getEntityTypeChanges() {
+    return Map.of(SIMPLE_ORGANIZATIONS_ID, COMPOSITE_ORGANIZATIONS_ID);
+  }
+
+  @Override
+  public Map<UUID, Map<String, String>> getFieldChanges() {
+    return Map.of(
+      SIMPLE_ORGANIZATIONS_ID,
+      Map.of("*", "organization.%s")
+    );
+  }
+}

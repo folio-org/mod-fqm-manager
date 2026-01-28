55e63ddd899ce656996d8f51407de40eb86d52a5
Noah Overcash <novercash@ebsco.com>
2026-01-27 19:21:53 -0500

    compile fix

--- src/main/java/org/folio/fqm/migration/types/SingleFieldMigrationResult.java
+++ src/main/java/org/folio/fqm/migration/types/SingleFieldMigrationResult.java
@@ -15,7 +15,7 @@ public record SingleFieldMigrationResult<F extends MigratableFqlField<F>>(
     return new SingleFieldMigrationResult<>(List.of(field), List.of(), false);
   }
   public static <T extends MigratableFqlField<T>> SingleFieldMigrationResult<T> removed() {
-    return new SingleFieldMigrationResult<>(List.of(), List.of(), false);
+    return new SingleFieldMigrationResult<T>(List.of(), List.of(), false);
   }
   public static <T extends MigratableFqlField<T>> SingleFieldMigrationResult<T> noop(T original) {
     return new SingleFieldMigrationResult<>(List.of(original), List.of(), false);

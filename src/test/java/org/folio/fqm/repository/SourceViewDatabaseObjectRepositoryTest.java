b35b8cd48224b06ed343d20c51509661885e32f2
Noah Overcash <novercash@ebsco.com>
2026-01-29 17:51:54 -0500

    oop

--- src/test/java/org/folio/fqm/repository/SourceViewDatabaseObjectRepositoryTest.java
+++ src/test/java/org/folio/fqm/repository/SourceViewDatabaseObjectRepositoryTest.java
@@ -4,7 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.lenient;

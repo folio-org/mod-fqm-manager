94b8ac2d44320383485d8e138449c119acef708a
Noah Overcash <novercash@ebsco.com>
2026-01-29 15:21:38 -0500

    fix testdb?

--- src/test/java/org/folio/fqm/context/TestDbSetupConfiguration.java
+++ src/test/java/org/folio/fqm/context/TestDbSetupConfiguration.java
@@ -14,6 +14,9 @@ import javax.sql.DataSource;
 import liquibase.exception.LiquibaseException;
 import liquibase.integration.spring.SpringLiquibase;
 import org.folio.fqm.client.SimpleHttpClient;
+import org.folio.fqm.config.MigrationConfiguration;
+import org.folio.fqm.migration.MigrationStrategyRepository;
+import org.folio.fqm.repository.CustomEntityTypeMigrationMappingRepository;
 import org.folio.fqm.repository.EntityTypeRepository;
 import org.folio.fqm.repository.SourceViewDatabaseObjectRepository;
 import org.folio.fqm.repository.SourceViewRecordRepository;
@@ -21,6 +24,7 @@ import org.folio.fqm.service.EntityTypeInitializationService;
 import org.folio.fqm.service.EntityTypeValidationService;
 import org.folio.fqm.service.MigrationService;
 import org.folio.fqm.service.SourceViewService;
+import org.folio.fqm.utils.JSON5ObjectMapperFactory;
 import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.liquibase.FolioSpringLiquibase;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -85,19 +89,25 @@ public class TestDbSetupConfiguration {
   static class EntityTypeInitializer {
 
     @Autowired
+    private CustomEntityTypeMigrationMappingRepository customEntityTypeMigrationMappingRepository;
+
+    @Autowired
     private EntityTypeRepository entityTypeRepository;
 
     @Autowired
     private EntityTypeValidationService entityTypeValidationService;
 
     @Autowired
-    private MigrationService migrationService;
+    private FolioSpringLiquibase liquibase;
 
     @Autowired
     private ResourcePatternResolver resourceResolver;
 
     @Autowired
-    private FolioSpringLiquibase liquibase;
+    private MigrationConfiguration migrationConfiguration;
+
+    @Autowired
+    private MigrationStrategyRepository migrationStrategyRepository;
 
     @Autowired
     private SourceViewDatabaseObjectRepository sourceViewDatabaseObjectRepository;
@@ -136,7 +146,14 @@ public class TestDbSetupConfiguration {
         null,
         entityTypeRepository,
         entityTypeValidationService,
-        migrationService,
+        new MigrationService(
+          customEntityTypeMigrationMappingRepository,
+          entityTypeRepository,
+          executionContext,
+          migrationConfiguration,
+          migrationStrategyRepository,
+          JSON5ObjectMapperFactory.create()
+        ),
         new SourceViewService(
           sourceViewDatabaseObjectRepository,
           sourceViewRecordRepository,

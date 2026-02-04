40c010dced9e8b7999166cd4e222683559206f80
Noah Overcash <novercash@ebsco.com>
2026-02-04 11:39:10 -0500

    more!

--- src/main/java/org/folio/fqm/migration/strategies/impl/V24Dot1TestForEmma.java
+++ src/main/java/org/folio/fqm/migration/strategies/impl/V24Dot1TestForEmma.java
@@ -20,6 +20,7 @@ public class V24Dot1TestForEmma extends AbstractSimpleMigrationStrategy {
 
   private static final UUID SIMPLE_USER_DETAILS = UUID.fromString("bb058933-cd06-4539-bd3a-6f248ff98ee2");
   private static final UUID SIMPLE_FOLIO_USER_DETAILS = UUID.fromString("f2615ea6-450b-425d-804d-6a495afd9308");
+  private static final UUID SIMPLE_BUDGET = UUID.fromString("71525bf3-ea51-47c4-bb18-7ab6f07d2b42");
 
   @Override
   public String getMaximumApplicableVersion() {
@@ -46,6 +47,11 @@ public class V24Dot1TestForEmma extends AbstractSimpleMigrationStrategy {
 
   @Override
   public Map<UUID, Map<String, String>> getFieldChanges() {
-    return Map.of(SIMPLE_FOLIO_USER_DETAILS, Map.of("email", "phone"));
+    return Map.of(
+      SIMPLE_FOLIO_USER_DETAILS,
+      Map.of("email", "phone"),
+      SIMPLE_BUDGET,
+      Map.of("updated_by_user_id", "updated_by_user_id2")
+    );
   }
 }
